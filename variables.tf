9b653a4dab84931fc40f3952917c88a29d800c47
Anton Savchuk <solaris24@gmail.com>
2023-06-22 14:21:57 +0300

    add same modules

--- variables.tf
+++ variables.tf
@@ -17,3 +17,26 @@ variable "GKE_NUM_NODES" {
   type        = number
   description = "GKE nodes number"
 }
+
+
+# Modules GITHUB and FLUX
+
+variable "GITHUB_OWNER" {
+  type        = string
+  description = "repository to use"
+}
+
+variable "GITHUB_TOKEN" {
+  type        = string
+  description = "Prsonal Github access token"
+}
+
+variable "FLUX_GITHUB_REPO" {
+  type        = string
+  description = "Flux GitOps repo"
+}
+
+variable "FLUX_GITHUB_TARGET_PATH" {
+  type        = string
+  description = "Flux manifests subdirectory"
+}
--- vars.tfvars
+++ vars.tfvars
@@ -2,3 +2,8 @@ GOOGLE_PROJECT   = "k3s-demo-363719"
 GOOGLE_REGION    = "us-central1-c"
 GKE_MACHINE_TYPE = "g1-small"
 GKE_NUM_NODES    = "2"
+
+FLUX_GITHUB_REPO = "flux-gitops"
+FLUX_GITHUB_TARGET_PATH = "clusters"
+GITHUB_OWNER = "solaris24251"
+# GITHUB_TOKEN = ""
